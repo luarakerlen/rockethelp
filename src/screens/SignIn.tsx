b12dad78c97c31eb4d40e565179c7478c126ff85
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-07-22 00:35:38 -0300

    recover name and password

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Heading, Icon, useTheme, VStack } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -6,7 +7,12 @@ import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 
 export function SignIn() {
+	const [name, setName] = useState('');
+	const [password, setPassword] = useState('');
+
 	const { colors } = useTheme();
+
+  function handleSignIn() {}
 
 	return (
 		<VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
@@ -20,14 +26,16 @@ export function SignIn() {
 				InputLeftElement={
 					<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
 				}
+				onChangeText={setName}
 			/>
 			<Input
 				mb={8}
 				placeholder='Senha'
 				InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
 				secureTextEntry
+				onChangeText={setPassword}
 			/>
-			<Button title='Entrar' w='full' />
+			<Button title='Entrar' w='full' onPress={handleSignIn} />
 		</VStack>
 	);
 }
