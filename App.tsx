a3a8b4ed07a8589eae9a0618c0c65b5150441728
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-07-21 23:46:12 -0300

    configure app.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { SignIn } from './src/screens/SignIn';
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <SignIn/>
-    </NativeBaseProvider>
-  );
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+	return (
+		<NativeBaseProvider theme={THEME}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+			{fontsLoaded ? <SignIn /> : <Loading />}
+		</NativeBaseProvider>
+	);
 }
