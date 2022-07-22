ed246401d3f826a661a1a8a20c571a3c2f15954b
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-07-22 00:24:46 -0300

    create sign in screen layout

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,33 @@
-import { Heading, VStack } from 'native-base';
+import { Heading, Icon, useTheme, VStack } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
 
-import Logo from '../assets/logo_primary.svg'
+import Logo from '../assets/logo_primary.svg';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
 
 export function SignIn() {
+	const { colors } = useTheme();
+
 	return (
 		<VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
-			<Logo/>
-      <Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
+			<Logo />
+			<Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
 				Acesse sua conta
 			</Heading>
+			<Input
+				mb={4}
+				placeholder='E-mail'
+				InputLeftElement={
+					<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+				}
+			/>
+			<Input
+				mb={8}
+				placeholder='Senha'
+				InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+				secureTextEntry
+			/>
+			<Button title='Entrar' w='full' />
 		</VStack>
 	);
 }
