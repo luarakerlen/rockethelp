6703f12b20482c2fabe19fd64a3faafcf5ce79e2
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-07-28 23:21:17 -0300

    create routes

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 	Roboto_400Regular,
 	Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import { Details, Home, Register, SignIn } from './src/screens';
+import { Routes } from './src/routes';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
 				backgroundColor='transparent'
 				translucent
 			/>
-			{fontsLoaded ? <Details /> : <Loading />}
+			{fontsLoaded ? <Routes /> : <Loading />}
 		</NativeBaseProvider>
 	);
 }
