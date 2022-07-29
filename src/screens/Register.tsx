1b1dd63682764fd998dcb41dd311a5fa7302b2a5
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-07-28 22:57:15 -0300

    update register screen

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,9 +1,19 @@
 import { VStack } from 'native-base';
+import { Button, Header, Input } from '../components';
 
 export function Register() {
-  return (
-    <VStack>
-
-    </VStack>
-  );
+	return (
+		<VStack flex={1} p={6} bg='gray.600'>
+			<Header title='Nova solicitação' />
+			<Input placeholder='Número do patrimônio' mt={4} />
+			<Input
+				placeholder='Descrição do problema'
+				flex={1}
+				mt={5}
+				multiline
+				textAlignVertical='top'
+			/>
+			<Button title='Cadastrar' mt={5} />
+		</VStack>
+	);
 }
