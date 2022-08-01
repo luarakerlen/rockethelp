4522926fed54825bade3c94d9149d788a3fa6cde
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-01 19:29:12 -0300

    update routes to show corretc route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+import { Loading } from '../components';
 import { SignIn } from '../screens';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+	useEffect(() => {
+		const subscriber = auth().onAuthStateChanged((response) => {
+			setUser(response);
+			setIsLoading(false);
+		});
+
+		return subscriber;
+	}, []);
+
+	if (isLoading) {
+		return <Loading />;
+	}
+
 	return (
 		<NavigationContainer>
-			<AppRoutes />
+			{user ? <AppRoutes /> : <SignIn />}
 		</NavigationContainer>
 	);
 }
