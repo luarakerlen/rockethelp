8109efeb32ea9e3402ab265a364485c0282cbede
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-02 16:23:50 -0300

    update sign up screen

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -80,7 +80,7 @@ export function SignUp() {
 		<VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
 			<Logo />
 			<Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
-				Acesse sua conta
+				Crie sua conta
 			</Heading>
 			<Input
 				mb={4}
